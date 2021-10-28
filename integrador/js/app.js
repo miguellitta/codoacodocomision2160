9a4bb4d2cec0eaca9b69bd76820cae0e0b6f6401
Miguel <miguel.litta@gmail.com>
2021-10-28 05:13:09 -0300

    ultima parte tp Integrador

--- integrador/js/app.js
+++ integrador/js/app.js
@@ -16,17 +16,17 @@ const comprarTickets=()=>{
                         </div>
                         <div class="card">
                             <div class="card-body border border-primary mr-1">
-                                <h5 class="card-title text-center">Estudiante</h5>
+                                <h5 class="card-title text-center">Trainee</h5>
                                 <p class="card-text text-center">Tienen un descuento</p>
-                                <p class="card-title text-center font-weight-bold">80%</p>
+                                <p class="card-title text-center font-weight-bold">50%</p>
                                 <p class="card-text text-center"><small class="text-muted">* presentar documentación</small></p>
                             </div>
                         </div>
                         <div class="card">
                             <div class="card-body border border-primary mr-1">
-                                <h5 class="card-title text-center">Estudiante</h5>
+                                <h5 class="card-title text-center">Junior</h5>
                                 <p class="card-text text-center">Tienen un descuento</p>
-                                <p class="card-title text-center font-weight-bold">80%</p>
+                                <p class="card-title text-center font-weight-bold">15%</p>
                                 <p class="card-text text-center"><small class="text-muted">* presentar documentación</small></p>
                             </div>
                         </div>
@@ -78,15 +78,89 @@ const comprarTickets=()=>{
                         </div>
                         <div class="form-row d-flex">
                             <div class="form-group col">
-                                <button type="reset" class="btn btn-codo btn-block">Borrar</button>
+                                <button type="reset" onclick="limpiar()" class="btn btn-codo btn-block">Borrar</button>
                             </div>
                             <div class="form-group col">
-                                <button type="button" id="botonCalcular" class="btn btn-codo btn-block">Resumen</button>
+                                <button type="button" id="botonCalcular" class="btn btn-codo btn-block" data-bs-toggle="modal" data-bs-target="#exampleModal">Resumen</button>
                             </div>
                         </div>
                     </form>
                 </div>
             </div>
         </div>
+        <!-- Modal -->
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">ticket N°</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div id="" class="modal-body">
+                
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+                <button type="button" class="btn btn-primary">Confirmar compra</button>
+            </div>
+            </div>
+        </div>
+        </div>
     `;
+    document.querySelector("#botonCalcular").addEventListener('click' ,()=>{
+        let inputs = document.querySelectorAll("input");
+        let descuento = document.querySelector("#inputState").value;
+        
+        console.log(inputs[0]);
+        let comprador={
+            nombre:inputs[0].value,
+            apellido:inputs[1].value,
+            correo:inputs[2].value,
+            cantidad:inputs[3].value
+        };
+        calcularValor(descuento, comprador.cantidad,comprador);
+    });
 };
+const calcularValor=(desc,cantEnt,comp)=>{
+    let resultado = 0;
+    let descuento = 0;
+    let numT = document.querySelector("#exampleModalLabel");
+    for (let index = 0; index < 8; index++) {
+        numT.innerHTML+=crearNumTicket();   
+    }
+    let modal = document.querySelector(".modal-body");
+    let alert = document.querySelector("#totalCompra");
+    if(desc == "estudiante"){
+        resultado = (cantEnt * 200)*0.2;
+        descuento = (cantEnt * 200)*0.8;
+        alert.innerHTML="Total a pagar: $";
+        alert.innerHTML+=resultado;
+    }else if(desc == "trainee"){
+        resultado = (cantEnt * 200)*0.5;
+        descuento = (cantEnt * 200)*0.5;
+        alert.innerHTML="Total a pagar: $";
+        alert.innerHTML+=resultado;
+    }else{
+        resultado = (cantEnt * 200)*0.85;
+        descuento = (cantEnt * 200)*0.15;
+        alert.innerHTML="Total a pagar: $";
+        alert.innerHTML+=resultado;
+    }
+    modal.innerHTML=`
+        <p>Nombre: ${comp.nombre}</p>
+        <p>Apellido: ${comp.apellido}</p>
+        <p>Correo: ${comp.correo}</p>
+        <p>Categoria: ${desc}</p>
+        <p>Cantidad de Entradas: ${comp.cantidad}</p>
+        <p>Descuento: ${descuento}</p>
+        <p>Total a pagar: ${resultado}</p>
+    `;
+}
+
+const crearNumTicket=()=>{
+    return Math.floor(Math.random()*9);
+};
+const limpiar=()=>{
+    let alert = document.querySelector("#totalCompra");
+    alert.innerHTML="Total a pagar: $";
+}
